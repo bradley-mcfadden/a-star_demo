87d6b36ab62814896a722a18c18451ecfe3fe92f
Bradley McFadden <bmcfadden00@gmail.com>
2019-08-26 22:31:43 -0400

    Added in some utility methods for creating PathFinderNodes, added an is_on_floor method, finished up get_neighbours.

--- CPUController.gd
+++ CPUController.gd
@@ -4,6 +4,7 @@ func _ready():
 	var pq: PriorityQueue = PriorityQueue.new()
 	for i in range(10):
 		pq.add(randi() % (i + 1), randi() % 10)
+#warning-ignore:unused_variable
 	for i in range(10):
 		pq.poll()
 	
@@ -40,6 +41,9 @@ class PathFinderNode:
 	func _init(xy:int, jump_value:int):
 		self.xy = xy
 		self.jump_value = jump_value
+	
+	func equals(node:PathFinderNode) -> bool:
+		return self.xy == node.xy && self.jump_value == node.jump_value
 
 class PathFinder:
 	var came_from:PoolByteArray
@@ -53,11 +57,13 @@ class PathFinder:
 		assert goal[0] < height && goal[0] > 0 && start[1] < height && start[1] > 0
 		self.goal = goal[1] << int(log(width) / (log(2))) | goal[0]
 		self.graph = graph
+#warning-ignore:unused_variable
 		var frontier:PriorityQueue = PriorityQueue.new()
 		self.width = width
 		self.height = height
 		came_from = []
 		var cost_so_far:PoolByteArray = []
+#warning-ignore:unused_variable
 		for i in range(width * height):
 			came_from.append(-1)
 			cost_so_far.append(-1)
@@ -66,15 +72,22 @@ class PathFinder:
 	func make_new_node(x:int, y:int, jv:int) -> PathFinderNode:
 		return PathFinderNode.new(y << int(log(width) / (log(2))) | x, jv)
 	
+	func make_new_nodev(xy:Vector2, jv:int) -> PathFinderNode:
+		return PathFinderNode.new(int(xy.y) << int(log(width) / (log(2))) | int(xy.x), jv)
+	
 	func distance(a:PathFinderNode, b:PathFinderNode) ->  int:
 		var distance:int = 0
+#warning-ignore:narrowing_conversion
 		distance += abs((a.xy & width) - (b.xy & width))
+#warning-ignore:narrowing_conversion
 		distance += abs((a.xy >> int(log(width) / log(2))) - (b.xy >> int(log(width) / log(2))))
 		return distance
 	
 	func heuristic(next:PathFinderNode) -> int:
 		var cost:int = 0
+#warning-ignore:narrowing_conversion
 		cost += abs((next.xy & width) - (goal & width))
+#warning-ignore:narrowing_conversion
 		cost += abs((next.xy >> int(log(width) / log(2))) - (goal >> int(log(width) / log(2))))
 		return cost
 	
@@ -83,18 +96,28 @@ class PathFinder:
 		var tpos:Vector2 = Vector2(node.xy & width, node.xy >> int(log(width) / log(2)))
 		if graph.get_cellv(tpos + Vector2(1, 0)) == 1 && (tpos + Vector2(0, 1)).x < width:
 			# add a new node to the right
-			# set each jump value equal to node.jump_value + 1
-			pass
+			neighbours.append(make_new_nodev(tpos + Vector2(1, 0), node.jump_value + 1))
 		if graph.get_cellv(tpos + Vector2(-1, 0)) == 1 && (tpos + Vector2(-1, 0)).x > 0:
-			# add a new node to the right
-			# set each jump value equal to node.jump_value + 1
-			pass
+			# add a new node to the left
+			neighbours.append(make_new_nodev(tpos + Vector2(-1, 0), node.jump_value + 1))
 		if graph.get_cellv(tpos + Vector2(0, 1)) == 1 && (tpos + Vector2(0, 1)).x < height:
-			# add a new node to the right
-			# set each jump value equal to node.jump_value + 1
-			pass
-		if graph.get_cellv(tpos + Vector2(1, 0)) == 1 && (tpos + Vector2(0,1)).x > 0:
-			# add a new node to the right
-			# set each jump value equal to node.jump_value + 1
-			pass
+			# add a new node to the bottom
+			neighbours.append(make_new_nodev(tpos + Vector2(1, 0), 
+					node.jump_value + 1 if (node.jump_value + 1) % 2 == 0 else node.jump_value + 2))
+		if graph.get_cellv(tpos + Vector2(0, -1)) == 1 && (tpos + Vector2(0, -1)).y > 0:
+			# add a new node to the top
+			neighbours.append(make_new_nodev(tpos + Vector2(-1, 0), 
+					node.jump_value + 1 if (node.jump_value + 1) % 2 == 0 else node.jump_value + 2))
+		for neighbour in neighbours:
+			if is_node_on_floor(neighbour) == true:
+				neighbour.jump_value = 0
 		return neighbours
+	
+	func is_tile_on_floor(position:Vector2) -> bool:
+		assert position.x < 0 && position.x > width && position.y < 0 && position.y > height
+		if graph.get_cellv(position + Vector2(0, 1)) == 1:
+			return true
+		return false
+	
+	func is_node_on_floor(node:PathFinderNode):
+		return is_tile_on_floor(Vector2(node.xy & width, (node.xy >> int(log(width) / log(2)))))
