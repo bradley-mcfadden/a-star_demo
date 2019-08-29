2acaf295cf6d08ee9a3689d3e57a69f06aa6d348
Bradley McFadden <bmcfadden00@gmail.com>
2019-08-28 21:52:02 -0400

    Creating paths through the editor interface now works. Next, add ability to move goal and start while program is running through use of mouse position and keys. Also, clean up unecessary bits of code.

--- CPUController.gd
+++ CPUController.gd
@@ -1,18 +1,25 @@
-extends Node
+extends Node2D
 const MAX_JUMP = 8
+export var WIDTH := 3
+export var HEIGHT := 3
+export var START:Vector2
+export var GOAL:Vector2
+onready var path:Array = []
 func _ready():
 	randomize()
-	var pf = PathFinder.new(get_parent(), Vector2(2, 2), Vector2(2, 0), 3, 3)
-	var path = pf.came_from[2].xy
-	var path2:Array = []
-	while true:
-		path2.append(path)
-		path = pf.came_from[path].xy
-		if pf.came_from[path].xy == 8:
-			break
-	print(path2)
-	
-		
+	var pf = PathFinder.new(get_parent(), START, GOAL, WIDTH, HEIGHT)
+	var next_step:PathFinderNode = pf.goal
+	path.append(pf.goal)
+	while next_step.xy != -1:
+		path.append(next_step)
+		next_step = pf.came_from[next_step.xy][next_step.jump_value]
+	for i in range(1, path.size()):
+		print(path[path.size()-i].to_string())
+	update()
+func _draw():
+	for i in range(0, path.size() - 1):
+		draw_line(Vector2(16, 16) + Vector2(path[i].xy % WIDTH, path[i].xy / WIDTH) * 32, Vector2(16, 16) + Vector2(path[i+1].xy % WIDTH, path[i+1].xy / WIDTH) * 32, Color(0, 0, 0))
+
 class PriorityQueue:
 	var data:Array
 	var priority:Array
@@ -48,44 +55,52 @@ class PathFinderNode:
 	
 	func equals(node:PathFinderNode) -> bool:
 		return self.xy == node.xy && self.jump_value == node.jump_value
+	
+	func to_string() -> String:
+		return "XY: " + str(xy) + " Jump Value:" + str(jump_value)
 
 class PathFinder:
 	var came_from:Array
 	var width:int
 	var height:int
-	var goal:int
+	var goal:PathFinderNode
 	var graph:TileMap
 	
-	func _init(graph:TileMap, start:Vector2, goal:Vector2, width:int, height:int):
+	func _init(graph:TileMap, start:Vector2, ggoal:Vector2, width:int, height:int):
 		assert start.x < width && start.x >= 0 && start.y < height && start.y >= 0
-		assert goal.x < width && goal.x >= 0 && goal.y < height && goal.y >= 0
-		self.goal = (goal.y * width) + goal.x
+		assert ggoal.x < width && ggoal.x >= 0 && ggoal.y < height && ggoal.y >= 0
 		self.graph = graph
 		self.width = width
 		self.height = height
 		#warning-ignore:unused_variable
 		var frontier:PriorityQueue = PriorityQueue.new()
-		frontier.add(make_new_node(start.x, start.y, 0), 0)
+		var start_node:PathFinderNode = make_new_node(start.x, start.y, 0)
+		self.goal = make_new_node(ggoal.x, ggoal.y, 0)
+		frontier.add(start_node, 0)
 		came_from = []
 		var cost_so_far:PoolByteArray = []
 		#warning-ignore:unused_variable
 		for i in range(width * height):
-			came_from.append(null)
+			came_from.append([])
+			for j in range(4 * height):
+				came_from[i].append(null)
 			cost_so_far.append(0)
+		came_from[start_node.xy][start_node.jump_value] = PathFinderNode.new(-1, -1)
 		cost_so_far[int(start.y) << int(log(width) / (log(2))) | int(start.x)] = 0
 		
 		while frontier.data.size() > 0:
 			var current:PathFinderNode = frontier.poll()
 			
-			if current.xy == self.goal:
+			if current.xy == self.goal.xy:
+				self.goal = current
 				break
 				
 			for next in neighbours(current):
-				if came_from.has(next) == false:
+				if came_from[next.xy][next.jump_value] == null:
 					var priority = heuristic(next)
 					frontier.add(next, priority)
-					came_from[next.xy] = current
-				if next.xy == self.goal:
+					came_from[next.xy][next.jump_value] = current
+				if next.xy == self.goal.xy:
 					print("Found the goal")
 		print()
 	
@@ -106,9 +121,9 @@ class PathFinder:
 	func heuristic(next:PathFinderNode) -> int:
 		var cost:int = 0
 		#warning-ignore:narrowing_conversion
-		cost += abs((next.xy & width) - (goal & width))
+		cost += abs((next.xy & width) - (goal.xy & width))
 		#warning-ignore:narrowing_conversion
-		cost += abs((next.xy >> int(log(width) / log(2))) - (goal >> int(log(width) / log(2))))
+		cost += abs((next.xy >> int(log(width) / log(2))) - (goal.xy >> int(log(width) / log(2))))
 		return cost
 	
 	func neighbours(node:PathFinderNode) -> Array:
